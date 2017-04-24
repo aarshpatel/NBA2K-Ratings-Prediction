1f346a25b3ff685bff8dc4079df6b13aaa96209e
Aarsh Patel <aarshpatel@umass.edu>
2017-04-24 16:24:30 -0400

    Added cross validation helper method in utils

--- Data_Analysis/Utils.py
+++ Data_Analysis/Utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from sklearn.metrics import make_scorer, mean_squared_error, mean_absolute_error
+from sklearn.model_selection import cross_val_score
 
 
 def root_mean_squared(act_y, pred_y):
@@ -21,3 +22,7 @@ mae_scorer_gs = make_scorer(absolute_error, greater_is_better=False)
 rmse_scorer_cv = make_scorer(root_mean_squared)
 mae_scorer_cv = make_scorer(absolute_error)
 
+def model_cross_validation(estimator, X, y, scoring_func, cv):
+	""" Returns the mean of all cross validation scores """
+	return np.mean(cross_val_score(estimator=estimator, X=X, y=y, scoring=scoring_func, cv=cv))
+	
