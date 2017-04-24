cf1c06a1a6aac3f59d08122047dc09d5a141c1b4
Aarsh Patel <aarshpatel@umass.edu>
2017-04-24 16:16:12 -0400

    Updated the utils file

--- Data_Analysis/Utils.py
+++ Data_Analysis/Utils.py
@@ -8,16 +8,16 @@ def root_mean_squared(act_y, pred_y):
     rmse = np.sqrt(mean_squared_error(act_y, pred_y))
     return rmse
 
-
 def absolute_error(act_y, pred_y):
     """ Mean Absolute Error"""
     mae = mean_absolute_error(act_y, pred_y)
     return mae
 
+# Scoring functions for GridSearch/RFE
+rmse_scorer_gs = make_scorer(root_mean_squared, greater_is_better=False)
+mae_scorer_gs = make_scorer(absolute_error, greater_is_better=False)
 
-def make_rmse_scorer():
-    return make_scorer(root_mean_squared, greater_is_better=False)
+# Scoring for regular cross validation (cross_val_score)
+rmse_scorer_cv = make_scorer(root_mean_squared)
+mae_scorer_cv = make_scorer(absolute_error)
 
-
-def make_mae_scorer():
-    return make_scorer(absolute_error, greater_is_better=False)
