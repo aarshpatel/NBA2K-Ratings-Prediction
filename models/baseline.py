18b99e2b2ee63d732f3228b0005c21cfe874c91e
Aarsh Patel <aarshpatel@umass.edu>
2017-04-29 21:14:50 -0400

    Added baseline model script

---
+++ models/baseline.py
@@ -0,0 +1,71 @@
+""" 
+Implementation of a baseline model for prediction of player ratings 
+The baseline model just predicts the mean of the player ratings. A good baseline 
+gives us a sense of what we are trying to acheive
+"""
+
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.metrics import make_scorer, mean_squared_error, mean_absolute_error
+from sklearn.model_selection import cross_val_score
+
+import numpy as np
+import pandas as pd
+
+
+
+class MeanBaselineModel(BaseEstimator, ClassifierMixin):  
+    """ Baseline Model - Predicts the mean of the ratings """
+
+    def __init__(self):
+        self.mean_ratings = None
+
+    def fit(self, X, y=None):
+        """
+        This should fit classifier. All the "work" should be done here.
+
+        Note: assert is not a good choice here and you should rather
+        use try/except blog with exceptions. This is just for short syntax.
+        """
+
+        self.mean_ratings = np.mean(y)
+
+        return self
+
+    def predict(self, X, y=None):
+        """ This should be called after fitting the model """
+        return np.asarray([self.mean_ratings for x in X])
+    
+    def absolute_error(self, act_y, pred_y):
+        """ Mean Absolute Error"""
+        mae = mean_absolute_error(act_y, pred_y)
+        return mae
+
+    def score(self, X, y=None):
+        """ 
+        Scoring Method for MeanBaseline Model 
+        Uses MAE (mean absolute error) to evaluate predictions
+        """
+
+        predictions = self.predict(X)
+        print("Predictions Shape: ", predictions.shape)
+        print("X shape: ", X.shape)
+        return self.absolute_error(y, predictions)
+
+# Loading in training data
+train = np.load('train.npy') 
+X = train[0:,0:-1]
+y = train[:, -1]
+
+X_train = np.load("X_train.npy")
+X_test = np.load("X_test.npy")
+y_train = np.load("y_train.npy")
+y_test = np.load("y_test.npy")
+
+  
+def absolute_error(act_y, pred_y):
+    """ Mean Absolute Error"""
+    mae = mean_absolute_error(act_y, pred_y)
+    return mae
+
+mae_scorer_cv = make_scorer(absolute_error)
+print "Average MAE: ", np.mean(cross_val_score(estimator=MeanBaselineModel(), X=X, y=y, scoring=mae_scorer_cv, cv=10))
