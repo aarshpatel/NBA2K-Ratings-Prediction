8137ae3fdba71822783ea1f0efa237cf6a4bccce
Bhavik Jain <bhavikjain358@gmail.com>
2017-04-29 21:46:28 -0400

    Changes to utils and added ridge_regression

--- Models/Utils.py
+++ Models/Utils.py
@@ -1,7 +1,13 @@
+""" 
+Implementation of the Utils file. This script contains all of the code that will be
+used in multiple scripts
+"""
+
 import numpy as np
 
 from sklearn.metrics import make_scorer, mean_squared_error, mean_absolute_error
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
 
 
 def root_mean_squared(act_y, pred_y):
@@ -25,4 +31,9 @@ mae_scorer_cv = make_scorer(absolute_error)
 def model_cross_validation(estimator, X, y, scoring_func, cv):
 	""" Returns the mean of all cross validation scores """
 	return np.mean(cross_val_score(estimator=estimator, X=X, y=y, scoring=scoring_func, cv=cv))
+
+def get_best_estimator(estimator, step, cv, scoring, parameters, X_train, y_train):
+    clf_mae = GridSearchCV(estimator=estimator, param_grid=parameters, cv=cv, scoring=scoring, n_jobs=-1, verbose=10)
+    clf_mae.fit(X_train, y_train)
+    return clf_mae.best_estimator_
 	
---
+++ Models/ridge_regression.py
@@ -0,0 +1,8 @@
+""" Experimenting with Ridge Regression on 2k dataset """
+
+import numpy as np
+
+from sklearn.linear_model import Ridge
+
+
+
