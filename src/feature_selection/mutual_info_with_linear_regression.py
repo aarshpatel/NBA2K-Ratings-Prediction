17d25edf69ad0dff3a71070462e3eee5db9ad99b
Aarsh Patel <aarshpatel@umass.edu>
2017-05-01 13:52:32 -0400

    mutual info feature selection and pipeline

--- src/feature_selection/mutual_info_with_linear_regression.py
+++
@@ -1,5 +0,0 @@
-""" 
-Applies feature selection to data using mutual information and
-runs a Linear Regression model on the new preprocessed data with the 
-best features according to mutual_information 
-"""
