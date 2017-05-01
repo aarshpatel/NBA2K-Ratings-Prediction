e086471e3188c7efbac20a8c0e3196de9bd6fce6
Bhavik Jain <bhavikjain358@gmail.com>
2017-05-01 13:53:39 -0400

    Working on run_me

---
+++ run_me.py
@@ -0,0 +1,3 @@
+from src.models import utils
+
+
--- src/models/linear_regression.py
+++ src/models/linear_regression.py
@@ -7,5 +7,6 @@ from utils import *
 
 baseline_linreg = LinearRegression()
 
-print "Average MAE (Linear Regression): ", model_cross_validation(LinearRegression(), X=X_all, y=y_all, scoring_func=mae_scorer_cv, cv=10)
+print "Average MAE with all features (Linear Regression): ", model_cross_validation(LinearRegression(), X=X_all, y=y_all, scoring_func=mae_scorer_cv, cv=10)
 # Average MAE 3.618
+print "Average MAE with only offensive features (Linear Regression): ", 
