9996dab6bca1f65b7806bcf88f4658e69aa36d7a
Bhavik Jain <bhavikjain358@gmail.com>
2017-05-01 13:56:17 -0400

    Added experiment for offensive defensive stats

--- src/models/linear_regression.py
+++ src/models/linear_regression.py
@@ -9,4 +9,7 @@ baseline_linreg = LinearRegression()
 
 print "Average MAE with all features (Linear Regression): ", model_cross_validation(LinearRegression(), X=X_all, y=y_all, scoring_func=mae_scorer_cv, cv=10)
 # Average MAE 3.618
-print "Average MAE with only offensive features (Linear Regression): ", 
+print "Average MAE with only offensive features (Linear Regression): ", model_cross_validation(LinearRegression(), X=train_offensive, y=y_all, scoring_func=mae_scorer_cv, cv=10)
+# Average MAE 3.63939107251
+print "Average MAE with only defensive features (Linear Regression): ", model_cross_validation(LinearRegression(), X=train_defensive, y=y_all, scoring_func=mae_scorer_cv, cv=10)
+# Average MAE 3.68146163841
